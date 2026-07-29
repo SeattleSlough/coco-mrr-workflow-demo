30a6026d33d46b8fe3e5ab56e9120388be145c25
Michael Maggs <michael@maggs.ai>
2026-07-28 20:43:00 -0700

    Add .gitignore for dbt build artifacts

--- target/compiled/saas_billing_project/models/staging/stg_cancellations.sql
+++
@@ -1,6 +0,0 @@
-select
-    cancellation_id,
-    subscription_id,
-    cancellation_date,
-    reason
-from SAAS_BILLING.RAW.cancellations
--- target/compiled/saas_billing_project/models/staging/stg_invoices.sql
+++
@@ -1,6 +0,0 @@
-select
-    invoice_id,
-    subscription_id,
-    invoice_date,
-    amount
-from SAAS_BILLING.RAW.invoices
